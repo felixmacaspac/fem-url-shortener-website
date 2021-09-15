9003b4e2ea292baac71092996c1d710e24161509
Felix <fmcmacaspac@protonmail.com>
2021-09-15 22:44:52 +0800

    🚀 build gulp compiler

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";var hamburger=document.getElementById("toggle-button"),navMenu=document.getElementById("nav-container");hamburger.addEventListener("click",(function(e){hamburger.classList.toggle("is_active"),navMenu.classList.toggle("is_active")}));
 //# sourceMappingURL=index.js.map
