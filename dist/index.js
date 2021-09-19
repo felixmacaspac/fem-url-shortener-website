52c6ab246aad87ae0738e42b04485461b288b1ac
Felix <fmcmacaspac@protonmail.com>
2021-09-19 09:00:29 +0800

    ⚡️improve the performance and fix bug in background-url

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
-"use strict";var hamburger=document.getElementById("toggle-button"),navMenu=document.getElementById("nav-container");hamburger.addEventListener("click",(function(e){hamburger.classList.toggle("is_active"),navMenu.classList.toggle("is_active")}));
+"use strict";
+var hamburger = document.getElementById("toggle-button"),
+    navMenu = document.getElementById("nav-container");
+hamburger.addEventListener("click", (function(e) {
+    hamburger.classList.toggle("is_active"), navMenu.classList.toggle("is_active")
+}));
 //# sourceMappingURL=index.js.map
