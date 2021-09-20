82382778e68c92ef56ba24982d411d272d9b6eec
Felix <fmcmacaspac@protonmail.com>
2021-09-20 17:26:20 +0800

    🚀 | compile gulp

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,2 @@
-"use strict";
-var hamburger = document.getElementById("toggle-button"),
-    navMenu = document.getElementById("nav-container");
-hamburger.addEventListener("click", (function(e) {
-    hamburger.classList.toggle("is_active"), navMenu.classList.toggle("is_active")
-}));
+"use strict";var hamburger=document.getElementById("toggle-button"),navMenu=document.getElementById("nav-container");hamburger.addEventListener("click",(function(e){hamburger.classList.toggle("is_active"),navMenu.classList.toggle("is_active")}));
 //# sourceMappingURL=index.js.map
